1066d3c02a9fb4d2f288bde62d6a50a2c4c25fd8
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-25 14:07:39 -0600

    wave system in place

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -8,6 +8,9 @@ public class WaveConfigSO : ScriptableObject
     [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawnTimeVariance;
+    [SerializeField] float minSpawnTime = 0.2f;
 
     public int GetEnemyCount()
     {
@@ -36,4 +39,12 @@ public class WaveConfigSO : ScriptableObject
     }
 
     public float GetMoveSpeed() { return moveSpeed; }
+
+    public float GetRandomSpawnTime()
+    {
+        float spawnTime = Random.Range(timeBetweenEnemySpawns - spawnTimeVariance,
+                                        timeBetweenEnemySpawns + spawnTimeVariance);
+
+        return Mathf.Clamp(spawnTime, minSpawnTime, float.MaxValue);
+    }
 }
